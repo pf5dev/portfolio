3908707f7e2346a358e1209ca7ae862a66104686
pf5dev <pauloafonseca14@outlook.com>
2025-01-15 21:52:01 -0300

    atualiado

--- autoetl.py
+++ autoetl.py
@@ -12,76 +12,68 @@ if platform.system()=='Windows':
 	os.system('cls')
 else:
 	os.system('clear')
-logging.basicConfig(level=logging.DEBUG)
-file_handler = logging.FileHandler('erro.log')
-file_handler.setLevel(logging.ERROR)
-logging.getLogger().addHandler(file_handler)
-sys.stderr = open('erro.log', 'w')
-try:
-	if platform.system()=='Windows':
-		path=r"c:%HOMEPATH%\Downloads"
-		u=os.path.expandvars(path)
-	else:
-		path=r"$HOME/Downloads"
-		u=os.path.expandvars(path)
-	v=[]
-	w=[]
-	for file in os.listdir(u):
-		if fnmatch.fnmatch(file,'*.csv'):
-				v.append(file)
-				for m in range(len(v)):
-					if platform.system()=='Windows':
-						w.append(u+"\x5c"+v[m])
-					w.append(u+"/"+v[m])
-	if len(w)>1:
-		w.pop(0)
+if platform.system()=='Windows':
+	path=r"c:%HOMEPATH%\Downloads"
+	u=os.path.expandvars(path)
+else:
+	path=r"$HOME/Downloads"
+	u=os.path.expandvars(path)
+v=[]
+w=[]
+for file in os.listdir(u):
+	if fnmatch.fnmatch(file,'*.csv'):
+		v.append(file)
+		for m in range(len(v)):
+			if platform.system()=='Windows':
+				w.append(u+"\x5c"+v[m])
+			w.append(u+"/"+v[m])
+if len(w)>1:
+	w.pop(0)
 #######################################################
-	class C1:
-		def __init__(self,*args):
-			self.a0='script'
-			self.a1='tabela'
-			self.a3=v
-			self.a4='text'
-		def nomecoluna(self,*args):
-				return i
-	c1=C1()
+class C1:
+	def __init__(self,*args):
+		self.a0='script'
+		self.a1='tabela'
+		self.a3=v
+		self.a4='text'
+	def nomecoluna(self,*args):
+		return i
+c1=C1()
 ########################################################
-	def f0():
-		l1=[]
-		for x in v:
-			with open(x,'rb') as f1:
-				data=f1.read()
-				x1=charset_normalizer.detect(data)
-				l1.append(data.decode(x1['encoding']))
-		for y in l1:
-			if ';' in y:
-				return ';'
-			else:
-				return ','
+def f0():
+	l1=[]
+	for x in v:
+		with open(x,'rb') as f1:
+			data=f1.read()
+			x1=charset_normalizer.detect(data)
+			l1.append(data.decode(x1['encoding']))
+			for y in l1:
+				if ';' in y:
+					return ';'
+				else:
+					return ','
 ########################################################
-	if platform.system()=='Windows':
-		os.chdir(u+'\x5c')
-	else:
-		os.chdir(u+'/')
-	for k in range(len(v)):
-		num_str=str(k)
-		with open(c1.a3[k],newline='',encoding='utf-8') as csvfile:
-				A=csv.reader(csvfile,delimiter=f'{f0()}')
-				for row in A:
-					pass
-					break						
-		# print(row)
-		with open(f'{c1.a0+num_str}.sql','w') as f:
-					f.write(f'CREATE TABLE {c1.a1+num_str}(')
-					for j,i in enumerate(row):
-						i=unidecode.unidecode(i)
-						if j == len(row)-1:
-							f.write(f'"{c1.nomecoluna(i)}" {c1.a4});\n')
-						else:								
-								f.write(f'"{c1.nomecoluna(i)}" {c1.a4},\n')
-					f.write(f"COPY {c1.a1+num_str} FROM '/docker-entrypoint-initdb.d/{c1.a3[k]}' DELIMITER \'{f0()}\' CSV HEADER;\n")
-except Exception as e:
-    logging.error(f"Um erro ocorreu: {str(e)}")
+if platform.system()=='Windows':
+	os.chdir(u+'\x5c')
+else:
+	os.chdir(u+'/')
+for k in range(len(v)):
+	num_str=str(k)
+	with open(c1.a3[k],newline='',encoding='utf-8') as csvfile:
+		A=csv.reader(csvfile,delimiter=f'{f0()}')
+		for row in A:
+			pass
+			break						
+	with open(f'{c1.a0+num_str}.sql','w') as f:
+		f.write(f'CREATE TABLE {c1.a1+num_str}(')
+		for j,i in enumerate(row):
+			i=unidecode.unidecode(i)
+			if j == len(row)-1:
+				f.write(f'"{c1.nomecoluna(i)}" {c1.a4});\n')
+			else:								
+				f.write(f'"{c1.nomecoluna(i)}" {c1.a4},\n')
+		f.write(f"COPY {c1.a1+num_str} FROM '/docker-entrypoint-initdb.d/{c1.a3[k]}' DELIMITER \'{f0()}\' CSV HEADER;\n")
+
 					
 
 
